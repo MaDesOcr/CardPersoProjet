7ae55c72381aa52ecd94b589b558d221291dd720
MaDesOcr <madesocr@gmail.com>
2025-10-22 14:07:24 +0200

    Add like

--- src/Component/Card.tsx
+++ src/Component/Card.tsx
@@ -1,12 +1,18 @@
+import {useState } from "react";
 import Perso from "../model/Perso";
 import "./card.css"
 
 function Card({perso} : {perso : Perso}){
+
+    const [compteur, setCompteur] = useState(0);
     return (
         <div className="card">
-            {perso.nomJoueur}
-            {perso.nomPerso}
-            {perso.score} 
+            <div>{perso.nomJoueur}</div>
+            <div>{perso.nomPerso}</div>
+            <div>{perso.score}</div>
+            
+            <div><button onClick={()=>{setCompteur(compteur+1)}}>Like</button> </div>
+            <div>{compteur}</div>
         </div>
     )
 
